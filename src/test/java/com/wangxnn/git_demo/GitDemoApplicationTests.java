7853eb5dc6318fbedd98b47fd375f72ffe1f3f15
wangx <252279128@qq.com>
2021-07-07 18:26:16 +0800

    update .gitignore

---
+++ src/test/java/com/wangxnn/git_demo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangxnn.git_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
